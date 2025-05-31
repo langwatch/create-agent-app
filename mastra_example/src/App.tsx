fad851ae74552b264381f8d0fef5502d07a2618c
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2025-05-31 12:12:24 +0200

    Move provider to the root

--- mastra_example/src/App.tsx
+++ mastra_example/src/App.tsx
@@ -1,20 +1,14 @@
 import { CopilotChat } from "@copilotkit/react-ui";
-import { CopilotKit } from "@copilotkit/react-core";
 import "@copilotkit/react-ui/styles.css";
 
 function App() {
   return (
-    <CopilotKit
-      runtimeUrl="http://localhost:4111/copilotkit"
-      agent="weatherAgent"
-    >
-      <CopilotChat
-        labels={{
-          title: "Your Assistant",
-          initial: "Hi! 👋 How can I assist you today?",
-        }}
-      />
-    </CopilotKit>
+    <CopilotChat
+      labels={{
+        title: "Your Assistant",
+        initial: "Hi! 👋 How can I assist you today?",
+      }}
+    />
   );
 }
 
--- mastra_example/src/main.tsx
+++ mastra_example/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { CopilotKit } from "@copilotkit/react-core";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CopilotKit
+      runtimeUrl="http://localhost:4111/copilotkit"
+      agent="weatherAgent"
+    >
+      <App />
+    </CopilotKit>
+  </StrictMode>
+);
